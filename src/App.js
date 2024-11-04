a945e48b8a6aa917bafa41e62d80172c93d47a3c
Jeevanprasath.E <e.jeevanprasath@gmail.com>
2024-11-04 15:31:35 +0530

    Project_added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyComponent from './component/page';
+
+const App = () => {
+  return (
+    <div>
+      <MyComponent />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './page.css'
+
+const Page = () => {
+  return (
+    <div className='Container'>
+      <h1>YOU CAN <br />HIRE FREELANCER <br />HERE</h1>
+      <p>It is long established fact that a reader will be distracted by the <br />readable content of the page.</p>
+      <button className='btn1'>About Us</button>
+      <button className='btn2'>Get A Quote</button>
+    </div>
+  );
+};
+
+export default Page;
